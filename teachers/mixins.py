574fca9d72ad244c614e0aeb4a92e813957a60d7
Shivam Thakur <shivamthakur02082005@gmail.com>
2024-08-08 00:43:10 +0530

    Removed

--- teachers/mixins.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-
-class TeacherTestMixin(UserPassesTestMixin):
-    def test_func(self):
-        return self.request.user.teachers is not None
